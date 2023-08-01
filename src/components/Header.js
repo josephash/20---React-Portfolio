a59708ff98a7579dda11b3aa62273af727316185
Sam Joseph <josephash1818@gmail.com>
2023-08-01 16:27:13 -0400

    header fix

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Nav from "./Nav";
+
+class Header extends Component {
+	render() {
+		return (
+			Nav.render
+		);
+	}
+}
+
+export default Header;
