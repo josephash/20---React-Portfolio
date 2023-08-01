7d58a7d36aadb5331b416f2399aba46c699411bb
Sam Joseph <josephash1818@gmail.com>
2023-08-01 14:56:24 -0400

    started

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Project from "./components/Project";
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<Nav />
+			<Project />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
